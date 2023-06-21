ce03f8f3eff9ecd34d2b5c6d28fd43fcf504be7a
antonionicolaci <antonionicolaci48@gmail.com>
2023-06-21 14:17:45 +0200

    add file

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+/*
+
+products
+    \__dogs
+    \__cats
+
+
+products: 
+    -costo iniziale
+    -sconto
+    -prezzo finale (tramite funzione)
+    -colore
+    -grandezza (sacco gende di croccantini)
+    -marca
+    -tipologia (cibo, vestiario, igiene)
+    -eta
+    -razza
+    -peso
+
+dogs:
+    -razza
+    -grandezza animale
+    -
+
+*/
+?>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pet Shop</title>
+</head>
+<body>
+    
+</body>
+</html>
